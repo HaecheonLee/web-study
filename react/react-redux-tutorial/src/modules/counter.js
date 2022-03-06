71c2c88efaf00062fe664d8286dc183b319e65d3
unknown <dlgocjs1@gmail.com>
2022-03-06 16:14:38 -0500

    react ch 17 - redux with react practice

---
+++ react/react-redux-tutorial/src/modules/counter.js
@@ -0,0 +1,29 @@
+// action type (module name/action name)
+const INCREASE = 'counter/INCREASE';
+const DECREASE = 'counter/DECREASE';
+
+// action creator
+export const increase = () => ({ type: INCREASE });
+export const decrease = () => ({ type: DECREASE });
+
+const initialState = {
+  number: 0,
+};
+
+// reducer
+function counter(state = initialState, action) {
+  switch(action.type) {
+    case INCREASE:
+      return {
+        number: state.number + 1,
+      };
+    case DECREASE:
+      return {
+        number: state.number - 1,
+      };
+    default:
+      return state;
+  }
+}
+
+export default counter;
